1a00d1447a526aa9cc1c4d1ac83fc1861d4a5bfd
kelly <kelly@restlater.com>
2015-11-29 15:21:46 -0800

    output run time

--- lib/team_effort.rb
+++ lib/team_effort.rb
@@ -8,20 +8,26 @@ module TeamEffort
     enumerable.each_with_index do |args, index|
       while pids.size == max_process_count
         finished_pid = Process.wait
-        pids.delete finished_pid
-        puts "TE finished:#{finished_pid}"
+        delete_pid(pids, finished_pid)
       end
 
-      pids << fork do
-        yield args
-      end
+      pids << [fork { yield args }, Time.now]
       item = index + 1
       puts "TE started:#{pids.last} ##{item} of #{total} (#{(item.to_f / total.to_f * 100).round(1)}%)"
     end
 
     while !pids.empty?
       finished_pid = Process.wait
-      pids.delete finished_pid
+      delete_pid(pids, finished_pid)
+    end
+  end
+
+  def self.delete_pid(pids, pid)
+    pids.delete_if do |p, t|
+      if p == pid
+        puts "TE finished:#{pid} in #{(Time.now - t).round(1)} seconds"
+        true
+      end
     end
   end
 end
