30d34a2efc10c4801f5c07e22feb1572814e3fb1
Kelly Felkins <kelly@spartascience.com>
2019-01-07 07:19:25 -0800

    Adds a raise in the parent when a child process generates an exception. Also reports the args that were used when the exception was raised.

--- lib/team_effort.rb
+++ lib/team_effort.rb
@@ -3,25 +3,44 @@ require_relative "team_effort/version"
 module TeamEffort
   def self.work(enumerable, max_process_count = 4, progress_proc: nil)
     pids = []
+    arg_sets = []
+
     max_count = enumerable.count
     completed_count = 0
 
-    enumerable.each do |args|
+    enumerable.each do |arg_set|
       while pids.size == max_process_count
-        finished_pid = Process.wait
-        pids.delete finished_pid
-        progress_proc.call(completed_count += 1, max_count) if progress_proc
+        pids, arg_sets, completed_count = wait_for_completion(pids, arg_sets, completed_count, max_count, progress_proc)
       end
 
       pids << fork do
-        yield args
+        begin
+          yield arg_set
+          exit! 0
+        rescue => e
+          $stderr.puts "TeamEffort child process error"
+          $stderr.puts e
+          $stderr.puts caller
+          exit! 1
+        end
       end
+      arg_sets << arg_set
     end
 
     while !pids.empty?
-      finished_pid = Process.wait
-      pids.delete finished_pid
-      progress_proc.call(completed_count += 1, max_count) if progress_proc
+      pids, arg_sets, completed_count = wait_for_completion(pids, arg_sets, completed_count, max_count, progress_proc)
     end
+  end
+
+  def self.wait_for_completion(pids, arg_sets, completed_count, max_count, progress_proc)
+    pid, status = Process.wait2
+    pids_index = pids.index(pid)
+    if pids_index
+      pids.delete_at pids_index
+      arg_set = arg_sets.delete_at pids_index
+      raise "TeamEffort child process failed when processing > #{arg_set} <" if !status.success?
+    end
+    progress_proc.call(completed_count += 1, max_count) if progress_proc
+    [pids, arg_sets, completed_count]
   end
 end
